fc8c93d8c30988b449b6282e3d9ac60c78a4d5b0
= <rav_bay@yahoo.com>
2016-03-09 16:44:40 +0800

    fixed unauth state on search

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -4,4 +4,14 @@ class Api::SearchController < ApiController
     render json: Gmap::MetaData.new(params.merge({user_id: current_user.id})).build
   end
 
+  private
+
+  def eval_params
+    if current_user.present?
+      params.merge({user_id: current_user.id})
+    else
+      params
+    end
+  end
+
 end
